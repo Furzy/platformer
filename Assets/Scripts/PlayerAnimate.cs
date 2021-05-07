0a3dad176be7c7e4fd3a52ee351b53d93b6ad4c7
Furzy <t.furzy@gmail.com>
2021-05-07 21:01:37 +0200

    FINALLY managed to get intro animation for Crouch

--- Assets/Scripts/PlayerAnimate.cs
+++ Assets/Scripts/PlayerAnimate.cs
@@ -21,16 +21,22 @@ public class PlayerAnimate : MonoBehaviour
         PLAYER_FJUMPEND
     }
 
-    private Animator anim;
+    [HideInInspector]
+    public Animator anim;
+    public AnimationState currentAnimationState = AnimationState.PLAYER_IDLE;
+
     private PlayerProperties pp;
     private SpriteRenderer sp;
     private PlayerGroundMovement pgm;
 
     public bool facingRight = true;
     public bool animationPlaying = false;
-    public float animationDelay = 0f;
+    public float animationLength = 0f;
 
-    private AnimationState currentAnimationState = AnimationState.PLAYER_IDLE;
+    private bool isKeyHolding = false;
+    private bool keyDown = false;
+    private bool keyHold = false;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -43,13 +49,12 @@ public class PlayerAnimate : MonoBehaviour
 
     private void Update()
     {
-        animationDelay = anim.GetCurrentAnimatorStateInfo(0).length;
-        GetCurrentAnimState();
+        SetNewAnimState();
         ChangeCurrentAnimState();
         Flip();
     }
 
-    private void GetCurrentAnimState()
+    private void SetNewAnimState()
 	{
         // On Ground
         if (pp.isGrounded)
@@ -57,12 +62,19 @@ public class PlayerAnimate : MonoBehaviour
             // No horizontal input
             if (Mathf.Abs(pp.direction.x) < 0.1f)
             {
-                if (pp.direction.y < -0.1f) // Crouching
+                if (Input.GetKeyDown(KeyCode.DownArrow)) // Crouch Start
                 {
-                
                     currentAnimationState = AnimationState.PLAYER_CROUCHSTART;
                 }
-                else // Idle
+                else if (Input.GetKeyUp(KeyCode.DownArrow)) // While Rising
+                {
+                    currentAnimationState = AnimationState.PLAYER_CROUCHEND;
+                }
+                else if (Input.GetKeyDown(KeyCode.UpArrow)) // Neutral Jumping
+                {
+                    currentAnimationState = AnimationState.PLAYER_NJUMPSTART;
+                }
+                else if (Mathf.Abs(pp.direction.y) < 0.1f) // Idle
                 {
                     currentAnimationState = AnimationState.PLAYER_IDLE;
                 }
@@ -80,19 +92,9 @@ public class PlayerAnimate : MonoBehaviour
                 }
             }
         }
-        // In Air
+        // In Air – WIP
         else
         {
-            // No horizontal input
-            if (Mathf.Abs(pp.direction.x) < 0.1f)
-            {
-
-            }
-            // Horizontal input
-            else 
-            {
-
-            }
         }
 	}
 
@@ -100,16 +102,17 @@ public class PlayerAnimate : MonoBehaviour
     {
         if (!animationPlaying)
         {
+
             switch (currentAnimationState)
             {
                 case AnimationState.PLAYER_CROUCHSTART:
                     anim.Play("Player_CrouchStart");
+                    animationLength = anim.GetCurrentAnimatorStateInfo(0).length - 0.8f;
                     animationPlaying = true;
-                    StartCoroutine(animationDelayer());
-                    if (!animationPlaying)
-                    {
-                        anim.Play("Player_Crouching");
-                    }
+                    StartCoroutine("WaitForAnimationEnd", animationLength);
+                    break;
+                case AnimationState.PLAYER_CROUCHING:
+                    anim.Play("Player_Crouching");
                     break;
                 default:
                     anim.Play("Player_Idle");
@@ -118,9 +121,10 @@ public class PlayerAnimate : MonoBehaviour
         }
     }
 
-    IEnumerator animationDelayer () 
-    {
-        yield return new WaitForSeconds (animationDelay);
+    IEnumerator WaitForAnimationEnd(float _animationLength)
+    { 
+        yield return new WaitForSeconds(_animationLength); 
+        currentAnimationState = AnimationState.PLAYER_CROUCHING;
         animationPlaying = false;
     }
 
@@ -136,6 +140,43 @@ public class PlayerAnimate : MonoBehaviour
             sp.flipX = true;
         }
     }
+
+    // private void HoldKey(KeyCode _key, float _animationLength)
+    // {
+    //     if (!keyDown) 
+    //     { 
+    //         if (Input.GetKeyDown(_key)) 
+    //         { 
+    //             StartCoroutine (CheckHoldKey()); 
+    //         } 
+    //     } 
+    //     else 
+    //     { 
+    //         if (Input.GetKey(_key) && !Input.GetKeyDown(_key)) 
+    //         { 
+    //             keyHold = true; 
+    //         } 
+    //     } 
+        
+    //     if (keyHold) 
+    //     { 
+    //         isKeyHolding = true;
+    //     } 
+        
+    //     if(Input.GetKeyUp(_key))
+    //     { 
+    //         keyHold = false; 
+    //         isKeyHolding = false;
+    //     } 
+
+    //     IEnumerator CheckHoldKey()
+    //     { 
+    //         keyDown = true; 
+    //         yield return new WaitForSeconds(_animationLength); 
+    //         keyDown = false; 
+    //     }
+    // }
+
 }
 
 
