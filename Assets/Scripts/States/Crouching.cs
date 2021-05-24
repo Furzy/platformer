6c5fd603b473edf42a3ebc92312305c92726ff9e
Furzy <t.furzy@gmail.com>
2021-05-24 17:47:11 +0200

    Regrouped CrouchStart and Crouching

--- Assets/Scripts/States/Crouching.cs
+++ Assets/Scripts/States/Crouching.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using UnityEngine;
 
+
 public class Crouching : State
 {
     public Crouching(PlayerScript playerScript, PlayerMovementScript playerMovementScript) : base (playerScript, playerMovementScript){}
+
+    private IEnumerator OnEnter(){
+            PlayerScript.Rb2d.velocity = new Vector2(0f, 0f);
+            PlayerScript.Animator.Play("CROUCH_START");
+
+            PlayerScript.isRecovered = false;
+            yield return new WaitUntil(() => PlayerScript.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f);
+            PlayerScript.isRecovered = true;  
+    }
+    
+    private IEnumerator OnMain(){
+            PlayerScript.Rb2d.velocity = new Vector2(0f, 0f);
+            PlayerScript.Animator.Play("CROUCHING");
+            yield break;
+    }
+
     public override IEnumerator Start()
     {
-        PlayerScript.Rb2d.velocity = new Vector2(0f, 0f);
-        PlayerScript.Animator.Play("CROUCHING");
-
-        yield break;
+        yield return OnEnter();
+        yield return OnMain();       
     }
 }
--- Assets/Scripts/States/Standing.cs
+++ Assets/Scripts/States/Standing.cs
@@ -13,4 +13,14 @@ public class Standing : State
         yield return new WaitUntil(() => PlayerScript.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f);
         PlayerScript.isRecovered = true;        
     }
+
+    // TODO: check this duplicate
+    public static IEnumerator ExecAnimation(PlayerScript playerScript){
+        playerScript.Rb2d.velocity = new Vector2(0f, 0f);
+        playerScript.Animator.Play("STANDING");
+
+        playerScript.isRecovered = false;
+        yield return new WaitUntil(() => playerScript.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f);
+        playerScript.isRecovered = true;  
+    }
 }
